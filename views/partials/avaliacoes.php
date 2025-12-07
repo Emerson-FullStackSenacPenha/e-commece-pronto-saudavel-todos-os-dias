e82ac9d0ecfd7acfcf543373a411f0db4d10ab1b
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-12-07 14:09:20 -0300

    Estilizando avaliações

--- views/partials/avaliacoes.php
+++ views/partials/avaliacoes.php
@@ -1,17 +1,134 @@
+<!-- Vai ser a posição do Avaliacões -->
+<section class="sessao_avaliacoes">
+    <h2>Avaliacões</h2>
+    <div class="carrossel">
+        <button class="btn_voltar" aria-label="Voltar avaliação">&lt;</button>
+        <div class="carrossel_avaliacoes">
 
-    <!-- Vai ser a posição do Avaliacões -->
-    <section class="sessao_avaliacoes">
-        <h2>Avaliacões</h2>
-        <div class="carrossel">
-            <button class="btn_voltar" aria-label="Voltar avaliação">&lt;</button>
-            <div class="carrossel_avaliacoes">
-                <article class="artigo_avaliacao"><p>A comida é de verdade, com aquele gostinho caseiro que faz toda a diferença na hora do almoço.</p></article>
-                <article class="artigo_avaliacao"><p>Pra quem busca praticidade sem perder a qualidade, eu super recomendo!</p></article>
-                <article class="artigo_avaliacao"><p>Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
-                <article class="artigo_avaliacao"><p>4 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
-                <article class="artigo_avaliacao"><p>5 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
-                <article class="artigo_avaliacao"><p>6 Ter uma refeição completa, balanceada e tão saborosa esperando por você é o melhor investimento no meu dia a dia.</p></article>
-            </div>
-            <button class="btn_avancar" aria-label="Proxima avaliação">&gt;</button>
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/1.svg" alt="Foto de Claudio">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Claudio</h3>
+                        <p class="dados-separados">31 anos | Personal Trainer</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                    
+
+                </div>
+
+                <p class="texto-depoimento">"Indico para todos os meus alunos! A Clau entende de dieta e as marmitas têm aquele gostinho de comida feita em casa, sem ser pesada. Minha favorita é a lasanha de abobrinha. O ponto alto é a pontualidade na entrega, sempre chega no prazo combinado, o que é essencial na minha rotina."</p>
+
+
+            </article>
+
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/2.svg" alt="Foto de Patricia">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Patricia</h3>
+                        <p class="dados-separados">22 anos | Universitária</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                </div>
+
+                <p class="texto-depoimento">"Gente, surreal como a Clau consegue deixar o brócolis gostoso! Sério, pedi o combo vegetariano da semana e veio super caprichado e o preço é bem ok para a minha mesada. Recomendo super para quem não tem tempo de cozinhar."</p>
+
+            </article>
+
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/3.svg" alt="Foto de Joana">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Joana</h3>
+                        <p class="dados-separados">40 anos | Empresária</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                </div>
+
+                <p class="texto-depoimento">"Melhor custo-benefício que encontrei na região! Compro o pacote mensal para o meu almoço e do meu marido, que é super chato com comida, e ele sempre elogia. A Clau sempre arrasa."</p>
+
+            </article>
+
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/4.svg" alt="Foto de Roberto">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Roberto</h3>
+                        <p class="dados-separados">67 anos | Aposentado</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                </div>
+
+                <p class="texto-depoimento">"Comecei a pedir por recomendação médica e estou impressionado. A porção é na medida certa, e o cuidado com a embalagem é excelente. O salmão grelhado estava perfeito. O melhor de tudo é a facilidade do site, em poucos cliques consigo montar meu pedido e finalizar a compra. Muito prático para a minha rotina. Recomendo demais!".</p>
+
+            </article>
+
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/5.svg" alt="Foto de Mateus">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Mateus</h3>
+                        <p class="dados-separados">19 anos | Estágiario</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                </div>
+
+                <p class="texto-depoimento">"Perfeitas para quem passa o dia inteiro estudando e trabalhando! As marmitas da Clau são muito mais práticas do que pedir fast food, e eu sei que estou comendo comida de verdade. A minha dieta para ganhar massa ficou muito mais fácil de seguir, ganhei tempo e saúde!"</p>
+
+            </article>
+
+            <article class="artigo_avaliacao">
+
+                <div class="perfil-info">
+
+                    <div id="perfil">
+                        <img class="fotoAvaliacao" src="../public/images/avaliacoes/6.svg" alt="Foto de Herminia">
+                    </div>
+
+                    <div class="dados-cliente">
+                        <h3 class="nome-cliente">Herminia</h3>
+                        <p class="dados-separados">85 anos | Aposentada</p>
+                        <p class="star">★★★★★</p>
+                    </div>
+
+                </div>
+
+                <p class="texto-depoimento">"Eu sou muito exigente com a minha comida, mas as marmitas da Clau me surpreenderam. É um alívio não ter que cozinhar todo dia! A comida é leve, e tem aquele sabor de casa que a gente não encontra mais. As embalagens são fáceis de abrir e as porções são ideais, uma ajuda e tanto para quem vive sozinha. Confio e recomendo."</p>
+
+            </article>
+
         </div>
-    </section>
+        <button class="btn_avancar" aria-label="Proxima avaliação">&gt;</button>
+    </div>
+</section>
